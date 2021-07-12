28eb2d59cc4754d72127375fd67acd6461f8e8d2
강인덕 <idk0919@naver.com>
2021-07-12 18:21:32 +0900

    fix: next version 11 -> 10

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -7,7 +7,7 @@ import Anchor from 'components/Anchor';
 const Logo: React.FC = () => {
   return (
     <Anchor href={HOME_PAGE}>
-      <Image src={LogoImage} alt="logo" key="logo" />
+      <Image src={LogoImage} alt="logo" key="logo" height="24" width="66" />
     </Anchor>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,21 @@
-import { Global } from '@emotion/react';
 import type { AppProps } from 'next/app';
+
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { Global } from '@emotion/react';
 import { GlobalStyle } from 'styles/global.style';
+
+// Create a client
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Global styles={GlobalStyle} />
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Global styles={GlobalStyle} />
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </>
   );
 }
